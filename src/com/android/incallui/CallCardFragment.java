07453a7a434f55c5279c1a59c4f20f43f9ff9e2b
kaiyiz <kaiyiz@codeaurora.org>
2014-10-23 10:17:49 +0800

    InCallUI: Stop the running animation before hide the icon
    There is a running alpha animation on the state icon,
    so it can be shown.
    
    Before hide the state icon, clear it's animation firstly
    and do not start animation unless it is visible.
    
    CRs-Fixed: 741096
    
    Change-Id: Id14d9f6e8e5d968b9d0497945833f7e6e78e1b06

--- src/com/android/incallui/CallCardFragment.java
+++ src/com/android/incallui/CallCardFragment.java
@@ -555,6 +555,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
             mCallStateLabel.setVisibility(View.VISIBLE);
 
             if (connectionIcon == null) {
+                mCallStateIcon.clearAnimation();
                 mCallStateIcon.setVisibility(View.GONE);
             } else {
                 mCallStateIcon.setVisibility(View.VISIBLE);
@@ -577,7 +578,9 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
                 mCallStateIcon.clearAnimation();
             } else {
                 mCallStateLabel.startAnimation(mPulseAnimation);
-                mCallStateIcon.startAnimation(mPulseAnimation);
+                if (mCallStateIcon.getVisibility() == View.VISIBLE) {
+                    mCallStateIcon.startAnimation(mPulseAnimation);
+                }
             }
         } else {
             mCallStateLabel.clearAnimation();

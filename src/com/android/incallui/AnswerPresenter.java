9c3fbd481889746441dd14b5a29a3d3db562a4cc
Kamaljeet Maini <kmaini@cyngn.com>
2016-04-22 14:43:25 -0700

    Add new options for call waiting response in InCallUI
    When an incoming call is received while there is already an active call,
    the user will be given four choices for the response:
    
    1) Left button: hold the current call and answer incoming call
    2) Bottom button: end the current call and answer the incoming call
    3) Right button: decline the incoming call
    4) Top button: Send a text message to the incoming caller if texting
       is enabled
    
    The changes in this component include updated icon graphics, display of text hints, and new APIs for additonal flag.
    
    Issue-Id: CYNGNOS-2749
    
    Change-Id: Ic97e0fde12ae98afa0124f4bb5326e874a146e91
    (cherry picked from commit f023f97370cfbe775a1fab04da3699e0f82c066b)

--- src/com/android/incallui/AnswerPresenter.java
+++ src/com/android/incallui/AnswerPresenter.java
@@ -439,7 +439,7 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
         return phoneId;
     }
 
-    public void onAnswer(int videoState, Context context) {
+    public void onAnswer(int videoState, Context context, int callWaitingResponseType) {
         int phoneId = getActivePhoneId();
         Log.i(this, "onAnswer  mCallId:" + mCallId + "phoneId:" + phoneId);
         if (mCallId == null || phoneId == -1) {
@@ -452,7 +452,8 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
             InCallPresenter.getInstance().acceptUpgradeRequest(videoState, context);
         } else {
             Log.d(this, "onAnswer (answerCall) mCallId=" + mCallId + " videoState=" + videoState);
-            TelecomAdapter.getInstance().answerCall(mCall[phoneId].getId(), videoState);
+            TelecomAdapter.getInstance().answerCall(mCall[phoneId].getId(), videoState,
+                    callWaitingResponseType);
         }
     }
 
@@ -554,6 +555,9 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
                 && mHasTextMessages;
         boolean withBlock = isBlockingEnabled();
 
+        Call activeCall = CallList.getInstance().getActiveCall();
+        boolean isCallWaiting = activeCall != null && activeCall != call;
+
         // Only present the user with the option to answer as a video call if the incoming call is
         // a bi-directional video call.
         if (call.isVideoCall(getUi().getContext())) {
@@ -575,14 +579,25 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
             }
         } else {
             if (withSms) {
-                getUi().showTargets(withBlock
-                        ? AnswerFragment.TARGET_SET_FOR_AUDIO_WITH_SMS_AND_BLOCK
-                        : AnswerFragment.TARGET_SET_FOR_AUDIO_WITH_SMS_WITHOUT_BLOCK);
-                getUi().configureMessageDialog(textMsgs);
+                if (isCallWaiting) {
+                    getUi().showTargets(AnswerFragment
+                            .TARGET_SET_FOR_AUDIO_WITH_SMS_AND_CALL_WAITING);
+                    getUi().configureMessageDialog(textMsgs);
+                } else {
+                    getUi().showTargets(withBlock
+                            ? AnswerFragment.TARGET_SET_FOR_AUDIO_WITH_SMS_AND_BLOCK
+                            : AnswerFragment.TARGET_SET_FOR_AUDIO_WITH_SMS_WITHOUT_BLOCK);
+                    getUi().configureMessageDialog(textMsgs);
+                }
             } else {
-                getUi().showTargets(withBlock
-                        ? AnswerFragment.TARGET_SET_FOR_AUDIO_WITHOUT_SMS_WITH_BLOCK
-                        : AnswerFragment.TARGET_SET_FOR_AUDIO_WITHOUT_SMS_AND_BLOCK);
+                if (isCallWaiting) {
+                    getUi().showTargets(AnswerFragment
+                            .TARGET_SET_FOR_AUDIO_WITHOUT_SMS_WITH_CALL_WAITING);
+                } else {
+                    getUi().showTargets(withBlock
+                            ? AnswerFragment.TARGET_SET_FOR_AUDIO_WITHOUT_SMS_WITH_BLOCK
+                            : AnswerFragment.TARGET_SET_FOR_AUDIO_WITHOUT_SMS_AND_BLOCK);
+                }
             }
         }
     }
